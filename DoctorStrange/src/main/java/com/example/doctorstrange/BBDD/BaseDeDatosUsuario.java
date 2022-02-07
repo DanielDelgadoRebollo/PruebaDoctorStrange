fcd610c67009debf16f046457612a1ce2a32b491
DanielDelgadoRebollo <72132961+DanielDelgadoRebollo@users.noreply.github.com>
2022-02-07 09:26:16 +0100

    up

--- DoctorStrange/src/main/java/com/example/doctorstrange/BBDD/BaseDeDatosUsuario.java
+++ DoctorStrange/src/main/java/com/example/doctorstrange/BBDD/BaseDeDatosUsuario.java
@@ -6,4 +6,14 @@ public interface BaseDeDatosUsuario {
     String DB_COL_USER = "Usuario";
     String DB_ATRIB_USERNAME = "Usuario";
     String DB_ATRIB_PASS = "Contrasena";
+    String DB_ATRIB_NAME = "Nombre";
+    String DB_ATRIB_APELLUDOS = "Apellidos";
+    String DB_ATRIB_LOCALIZACION = "Localizacion";
+    String DB_ATRIB_EDAD = "Edad";
+    String DB_ATRIB_EMAIL = "Email";
+    String DB_ATRIB_FECHALTA = "FechaAlta";
+    String DB_ATRIB_SEX = "Sexo";
+    String DB_ATRIB_TARJETA_BANCARIA = "TarjetaBancaria";
+    String DB_ATRIB_TRABAJO = "Trabajo";
+    String DB_ATRIB_NACIONALIDAD = "Nacionalidad";
 }
